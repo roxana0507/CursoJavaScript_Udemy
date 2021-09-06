7916b00a1c020fbcc1320f377866b8a18c9e6956
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 14:41:42 -0600

    Operator assignment in JS
    Operator assignment in JS

---
+++ Leccion03/OperadoresAsignacion.js
@@ -0,0 +1,17 @@
+let a = 1 // el = es el operador de asignacion
+
+
+a += 3;// operador compuesto, es igual a decir a = a + 3
+console.log(a);
+
+a -= 1;
+console.log(a);
+
+a *= 5;
+console.log(a);
+
+a /= 5;
+console.log(a);
+
+a %= 2;
+console.log(a);
