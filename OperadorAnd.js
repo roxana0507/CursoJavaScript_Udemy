ca10b43a73fe7f9c6a544b118e8c27c105cbbeff
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 14:58:50 -0600

    And operator in JS
    And operator in JS

---
+++ OperadorAnd.js
@@ -0,0 +1,21 @@
+// sirve para agregar o validar dos condiciones
+let a = 5;
+let valMin = 0, valMax = 10;
+
+if (a >= valMin && a <= valMax) {
+    console.log("Dentro del rango");
+}
+else{
+    console.log("Fuera de rango");
+}
+
+let b = -1;
+let _valMin = 0, _valMax = 10;
+
+if (b >= _valMin && a <= _valMax) {
+    console.log("Dentro del rango");
+}
+else{
+    console.log("Fuera de rango");
+}
+//con alguna de ella que sea falso regresa falso.Deben cumplirse ambas 
