030f9fc3ce4a00ea32dd72d668bcb2efb879219e
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 15:21:26 -0600

    Ternary operator in JS
    Ternary operator in JS

---
+++ OperadorTernario.js
@@ -0,0 +1,24 @@
+//En este una parte será verdadera y otra será falsa.
+let resultado = (3 > 2) ? "verdadero" : "falso";
+console.log(resultado);
+
+let resultado1 = (1 > 2) ? "verdadero" : "falso";
+console.log(resultado1);
+
+let resultado2 = (3 > 2) ? 1 : 2;
+console.log(resultado2);
+
+//Ejemplo de numeros par con operador ternario
+//NUMERO PAR
+let a = 10
+if ( a % 2 == 0 ) {
+    console.log("Es un numero par");
+}
+else {
+    console.log("Es un numero impar");
+}
+
+//TERNARIO
+let numero = 10;
+resul = (numero %2 == 0) ? "Numero par" : "Numero impar";
+console.log(resul);
