fd8e05fc6d74e92ad189b59093d797a325b0effd
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 16:51:09 -0600

    If-else control statement example
    If-else control statement example

---
+++ Leccion04/SentenciaControl_IfElse.js
@@ -0,0 +1,18 @@
+let condicion = false;
+
+if (condicion) {
+  console.log("condicion verdadera");
+}
+else {
+  console.log("condicion falsa");
+}
+
+let condicion1 = true;
+
+if (3>2) {
+  console.log("condicion verdadera");
+}
+else {
+  console.log("condicion falsa");
+}
+
