a3a99359843e464d0d4bfabe146baa3df4c00566
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 14:48:50 -0600

    Add example less than
    Add example less than

--- Leccion03/EjemploMayorEdad.js
+++ Leccion03/EjemploMayorEdad.js
@@ -1,5 +1,15 @@
+//Mayor que
 let edad = 20, adulto= 18;
 if (edad >= 18){
+    console.log("Es un adulto");
+}
+else{
+    console.log("Es menor de edad");
+}
+
+//Menor que
+let edad2 = 17, adulto2= 18;
+if (edad2 >= 18){
     console.log("Es un adulto");
 }
 else{
