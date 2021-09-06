91ad76e289258ab4cf5754b65f2d2679b49075aa
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 16:59:04 -0600

    If else if control statement example
    If else if control statement example

--- Leccion04/SentenciaControl_IfElse.js
+++ Leccion04/SentenciaControl_IfElse.js
@@ -15,4 +15,3 @@ if (3>2) {
 else {
   console.log("condicion falsa");
 }
-
---
+++ Leccion04/SentenciaControl_IfElseIf.js
@@ -0,0 +1,17 @@
+let numero = -1;
+
+if(numero ==1){
+    console.log("Numero 1");
+}
+else if(numero ==2){
+    console.log("Numero 2");
+}
+else if(numero ==3){
+    console.log("Numero 3");
+}
+else if(numero ==4){
+    console.log("Numero 4");
+}
+else{
+    console.log("Numero desconocido");
+}
