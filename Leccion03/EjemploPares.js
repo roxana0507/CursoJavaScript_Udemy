7ea10cf67119e4849b41a48b8d1583086f9337c1
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 14:42:49 -0600

    Example even numbers in JS
    Example even numbers in JS

---
+++ Leccion03/EjemploPares.js
@@ -0,0 +1,17 @@
+//NUMERO PAR
+let a = 10
+if ( a % 2 == 0 ) {
+    console.log("Es un numero par");
+}
+else {
+    console.log("Es un numero impar");
+}
+
+// NUMERO IMPAR
+let b = 9
+if ( b % 2 == 0 ) {
+    console.log("Es un numero par");
+}
+else {
+    console.log("Es un numero impar");
+}
