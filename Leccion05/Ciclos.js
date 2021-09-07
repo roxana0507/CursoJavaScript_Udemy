2839838e970f65f4848c5ae2b9b5c60d8359f2ec
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-06 21:44:32 -0600

    Cycle do while
    Cycle do while

--- Leccion05/Ciclos.js
+++ Leccion05/Ciclos.js
@@ -1,4 +1,4 @@
-//WHILE
+//WHILE, reisa de primero la condicion sino se cumple no se ejecuta. Hace la valoracion antes de ejecutar.
 let contador = 0;
 
 while( contador < 3){
@@ -6,3 +6,14 @@ while( contador < 3){
     contador++;
 }
 console.log("Fin del ciclo while");
+
+//DOWHILE se diferencia del while porque este ciclo al menos se ejecuta una vez si es falsa termina y si es verdadera se vuelve a repetir.Hace la valoracion despues de ejecutar.
+
+let contador1 = 0;
+
+do{
+    console.log(contador1);
+    contador1++;
+}
+while( contador1 < 3);
+console.log("Fin del ciclo do while");
