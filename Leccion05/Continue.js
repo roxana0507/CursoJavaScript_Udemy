0cf165ebb1ca268838567339b0e86a26cf697b7e
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-07 14:49:47 -0600

    Labels in JS
    Labels in JS

--- Leccion05/Continue.js
+++ Leccion05/Continue.js
@@ -7,4 +7,16 @@ for (let contador = 0; contador <= 10; contador++ ){
     }
 }
 //si la variable de contador no es un numero par se activa la opcion de continue y pasa a la siguiente iteracion.
-// En las iteraciones en donde contador es igual a par no se jecuta el continue y se imprime en consola la variable de contador. Continue rompe la iteracion pero break rompe el ciclo. 
+// En las iteraciones en donde contador es igual a par no se jecuta el continue y se imprime en consola la variable de contador. Continue rompe la iteracion pero break rompe el ciclo.
+
+//USO DE ETIQUETAS/ LABELS
+inicio:
+for (let contador = 0; contador <= 10; contador++ ){
+    if(contador % 2 !== 0){
+        continue inicio; //ir a la siguiente iteración
+    }
+    else{
+        console.log(contador);
+    }
+}
+// Con la etiqueta inicio le indico alprograma donde ejecutarse. No es recomendado usarlas.
