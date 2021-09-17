bf4fca9e8e74d6185625cf0f48c2f2255a40792c
Roxana Chinchilla <roxy.0507@gmail.com>
2021-09-16 18:03:00 -0600

    Ways to create objects in JS
    Ways to create objects in JS

--- Leccion08/FormasCrearObjetos.js
+++ Leccion08/FormasCrearObjetos.js
@@ -1,4 +1,20 @@
+let miObjeto = new Objeto();
+let miObjeto2 = {}; //esta es la forma mas recomendada
 
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola'; //esta es la forma mas recomendada
+
+let miNumero1 = new Number(1);
+let miNumero2 = 1;//esta es la forma mas recomendada
+
+let miBoolean1 = new Boolean(false);
+let miBoolean2 = false;//esta es la forma mas recomendada
+
+let miArreglo1 = new Array();
+let miArreglo2 = []; //esta es la forma mas recomendada
+
+let miFuncion1 = new Function()
+let miFuncion2 = function name(params) {}//esta es la forma mas recomendada
 
 
 
